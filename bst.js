a93b8320cebfd4db19dd00c3ea0216bde070d23c
S2reds <110210946+S2reds@users.noreply.github.com>
2023-03-29 17:44:59 -0700

    +inOrder/postOrder & insert method

--- bst.js
+++ bst.js
@@ -1,15 +1,23 @@
 import node from './node.js'
 
 
-export default function tree(arr) {
+export  function tree(arr) {
     let sorted = sortAndDup(arr)
     let root = arrayToBST(sorted, 0, sorted.length - 1)
-    let preOrdered = preOrder(root)
+    let preOrdered = () => {
+        return preOrder(root)
+    }
+    let inOrdered = () => {
+        return inOrder(root)
+    }
+    let postOrdered = () => {
+        return postOrder(root)
+    }
+        
 
     function arrayToBST(arr, start, end) {
         if (start > end) return null
         let mid = Math.floor((start+end)/2)
-        console.log(mid)
         let root = node(arr[mid])
         // recursively construct left subtree
         root.left = arrayToBST(arr, start, mid - 1)
@@ -27,16 +35,66 @@ export default function tree(arr) {
     }
     function preOrder(node) {
         if (node === null) return
-        document.write(node.val + " ")
-        preOrder(node.left)
-        preOrder(node.right)
+        let arr = [node.val, preOrder(node.left), preOrder(node.right)].flatMap(num => {
+            if (num === undefined) return null
+            else return num
+        })
+        return arr.filter(Boolean)     
     }
-
+    function inOrder(node) {
+        if (node === null) return
+        let arr = [inOrder(node.left), node.val, inOrder(node.right)].flatMap(num => {
+            if (num === undefined) return null
+            else return num
+        })
+        return arr.filter(Boolean)     
+    }
+    function postOrder(node) {
+        if (node === null) return
+        let arr = [ postOrder(node.left), postOrder(node.right), node.val].flatMap(num => {
+            if (num === undefined) return null
+            else return num
+        })
+        return arr.filter(Boolean)   
+    }
+    function insert(value) {
+        let newNode = node(value)
+        let prev = null
+        let temp = root
+        while (temp) {
+            if (temp.val > value) {
+                prev = temp
+                temp = temp.left
+            } else if (temp.val < value) {
+                prev = temp
+                temp = temp.right
+            }
+        }
+        if (prev.val > value) prev.left = newNode
+        else if (prev.val < value) prev.right = newNode
+        preOrdered = preOrder(root)
+        inOrdered = inOrder(root)
+        postOrdered = postOrder(root)
+    }
     return {
         root,
-        arrayToBST,
-        sortAndDup,
         sorted,
-        preOrdered
+        preOrdered,
+        inOrdered,
+        postOrdered,
+        insert
+    }
+}
+
+export function prettyPrint(node, prefix = '', isLeft = true) {
+    if (node === null) {
+       return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.val}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
 }
--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-import tree from './bst.js'
+import {tree, prettyPrint} from './bst.js'
 
 let arr = [6,6,4,6,1,7,8,9,4,5,3,3,3,3,3]
 
 let newTree = tree(arr)
 console.log(newTree.root)
 console.log(newTree.sorted)
-console.log(newTree.preOrdered)
+console.log(newTree.preOrdered())
+console.log(newTree.inOrdered())
+console.log(newTree.postOrdered())
+newTree.insert(12)
+prettyPrint(newTree.root)
+console.log(newTree.sorted)
+console.log(newTree.preOrdered())
+console.log(newTree.inOrdered())
+console.log(newTree.postOrdered())
+newTree.insert(1000)
+prettyPrint(newTree.root)
